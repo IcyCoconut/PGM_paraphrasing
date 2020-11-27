e5de8fb5938f0ae14c7512755aa6f6e5a0c1053d
IcyCoconut <43014432+IcyCoconut@users.noreply.github.com>
2020-11-26 22:39:52 -0700

    comment out pythonrouge

--- evaluate.py
+++ evaluate.py
@@ -8,7 +8,7 @@ import numpy as np
 #from bert.bertinterface import BertEncoding, BertSimilarity
 #from rouge import Rouge
 #import rouge
-from pythonrouge.pythonrouge import Pythonrouge
+#from pythonrouge.pythonrouge import Pythonrouge
 import rouge
 
 class Option(object):
@@ -190,67 +190,67 @@ def evaluate_semantic(reference_path, generated_path):
     summation = torch.sum(rep1*rep2,1)/(rep1.norm(2,1)*rep2.norm(2,1))
     print("semantic", torch.mean(summation))
 
-def evaluate_rouge(reference_path, generated_path, multi=True):
-    # Evaluate model scores
-    actual_word_lists = []
-    multi_flag = True
-    with open(reference_path) as f:
-        for line in f:
-            if multi:
-                sents = line.strip().lower().split('#')
-                actual_word_lists.append([x for x in sents])
-            else:
-                actual_word_lists.append([line.strip().lower()])
-    generated_word_lists = []
-    with open(generated_path) as f:
-        for line in f:
-            generated_word_lists.append(line.strip().lower())
-    actual_word_lists = actual_word_lists[:len(generated_word_lists)]
-    print(actual_word_lists[0],len(generated_word_lists))
+# def evaluate_rouge(reference_path, generated_path, multi=True):
+#     # Evaluate model scores
+#     actual_word_lists = []
+#     multi_flag = True
+#     with open(reference_path) as f:
+#         for line in f:
+#             if multi:
+#                 sents = line.strip().lower().split('#')
+#                 actual_word_lists.append([x for x in sents])
+#             else:
+#                 actual_word_lists.append([line.strip().lower()])
+#     generated_word_lists = []
+#     with open(generated_path) as f:
+#         for line in f:
+#             generated_word_lists.append(line.strip().lower())
+#     actual_word_lists = actual_word_lists[:len(generated_word_lists)]
+#     print(actual_word_lists[0],len(generated_word_lists))
 
 
-    for aggregator in ['Avg', 'Best', 'Individual']:
-        print('Evaluation with {}'.format(aggregator))
-        apply_avg = aggregator == 'Avg'
-        apply_best = aggregator == 'Best'
+#     for aggregator in ['Avg', 'Best', 'Individual']:
+#         print('Evaluation with {}'.format(aggregator))
+#         apply_avg = aggregator == 'Avg'
+#         apply_best = aggregator == 'Best'
         
-        evaluator = rouge.Rouge(metrics=['rouge-n', 'rouge-l', 'rouge-w'],
-                               #max_n=4,
-                               limit_length=True,
-                               length_limit=1000,
-                               length_limit_type='words',
-                               apply_avg=apply_avg,
-                               apply_best=apply_best,
-                               alpha=0.5, # Default F1_score
-                               weight_factor=1.2,
-                               stemming=True)
-        all_hypothesis  = generated_word_lists
-        all_references = actual_word_lists
-        scores = evaluator.get_scores(all_hypothesis, all_references)
+#         evaluator = rouge.Rouge(metrics=['rouge-n', 'rouge-l', 'rouge-w'],
+#                                #max_n=4,
+#                                limit_length=True,
+#                                length_limit=1000,
+#                                length_limit_type='words',
+#                                apply_avg=apply_avg,
+#                                apply_best=apply_best,
+#                                alpha=0.5, # Default F1_score
+#                                weight_factor=1.2,
+#                                stemming=True)
+#         all_hypothesis  = generated_word_lists
+#         all_references = actual_word_lists
+#         scores = evaluator.get_scores(all_hypothesis, all_references)
 
-        for metric, results in sorted(scores.items(), key=lambda x: x[0]):
-            if not apply_avg and not apply_best: # value is a type of list as we evaluate each summary vs each reference
-                for hypothesis_id, results_per_ref in enumerate(results):
-                    nb_references = len(results_per_ref['p'])
-                    for reference_id in range(nb_references):
-                        print('\tHypothesis #{} & Reference #{}: '.format(hypothesis_id, reference_id))
-                        print('\t' + prepare_results(metric,results_per_ref['p'][reference_id], results_per_ref['r'][reference_id], results_per_ref['f'][reference_id]))
-                print()
-            else:
-                print(prepare_results(metric, results['p'], results['r'], results['f']))
-
-
+#         for metric, results in sorted(scores.items(), key=lambda x: x[0]):
+#             if not apply_avg and not apply_best: # value is a type of list as we evaluate each summary vs each reference
+#                 for hypothesis_id, results_per_ref in enumerate(results):
+#                     nb_references = len(results_per_ref['p'])
+#                     for reference_id in range(nb_references):
+#                         print('\tHypothesis #{} & Reference #{}: '.format(hypothesis_id, reference_id))
+#                         print('\t' + prepare_results(metric,results_per_ref['p'][reference_id], results_per_ref['r'][reference_id], results_per_ref['f'][reference_id]))
+#                 print()
+#             else:
+#                 print(prepare_results(metric, results['p'], results['r'], results['f']))
 
 
-        rouge = Pythonrouge(summary_file_exist=False,
-                        summary=generated_word_lists, reference=actual_word_lists,
-                        n_gram=2, ROUGE_SU4=True, ROUGE_L=False,
-                        recall_only=True, stemming=True, stopwords=True,
-                        word_level=True, length_limit=True, length=50,
-                        use_cf=False, cf=95, scoring_formula='average',
-                        resampling=True, samples=1000, favor=True, p=0.5)
-        score = rouge.calc_score()
-        print("rouge: ", score)
+
+
+#         rouge = Pythonrouge(summary_file_exist=False,
+#                         summary=generated_word_lists, reference=actual_word_lists,
+#                         n_gram=2, ROUGE_SU4=True, ROUGE_L=False,
+#                         recall_only=True, stemming=True, stopwords=True,
+#                         word_level=True, length_limit=True, length=50,
+#                         use_cf=False, cf=95, scoring_formula='average',
+#                         resampling=True, samples=1000, favor=True, p=0.5)
+#         score = rouge.calc_score()
+#         print("rouge: ", score)
 
 def prepare_results(m, p, r, f):
     return '\t{}:\t{}: {:5.2f}\t{}: {:5.2f}\t{}: {:5.2f}'.format(m, 'P', 100.0 * p, 'R', 100.0 * r, 'F1', 100.0 * f)
